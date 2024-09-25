7cf661bc415f574524a0e8c1009ab596250adb7f
Sedat Doğan <dogannnsedat@gmail.com>
2024-09-25 19:03:18 +0300

    Somut olan Read ve Write repoları oluşturuldu , temel ayarları yapıldı

---
+++ Core/DataAcces/EfCoreRepository/ReadRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAcces.EfCoreRepository
+{
+    public class ReadRepository<T, TContext> : IReadRepository<T>
+    where T : BaseEntity<Guid>,
+    new() where TContext : DbContext
+    {
+        private readonly TContext _context;
+
+        public ReadRepository(TContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> GetTable => throw new NotImplementedException();
+    }
+}
---
+++ Core/DataAcces/EfCoreRepository/WriteRepository.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.DataAcces.EfCoreRepository
+{
+    public class WriteRepository<T, TContext> : IRepository<T>
+        where T : BaseEntity<Guid>,
+        new() where TContext : DbContext
+    {
+        private readonly TContext _context;
+
+        public WriteRepository(TContext context)
+        {
+            _context = context;
+        }
+
+        public DbSet<T> GetTable => throw new NotImplementedException();
+    }
+}
