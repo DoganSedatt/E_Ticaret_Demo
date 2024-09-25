00136189fcf7357f30e04a5526c651b3fe05af4c
Sedat Doğan <dogannnsedat@gmail.com>
2024-09-25 18:33:04 +0300

    BaseEntity Updated

---
+++ Core/Entity/BaseEntity.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entity
+{
+    public abstract class BaseEntity<TEntityId>
+    {
+        public TEntityId Id { get; set; }
+        public DateTime CreatedDate { get; set; }
+        public DateTime UpdatedDate { get; set; }
+        public DateTime DeletedDate { get; set; }
+    }
+}
