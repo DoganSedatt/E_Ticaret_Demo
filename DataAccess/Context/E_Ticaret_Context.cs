c5ee387963c1817586a0a9636e9f6c508321bfda
Sedat Doğan <dogannnsedat@gmail.com>
2024-09-25 18:48:36 +0300

    Context sınıfı oluşturuldu ve servis kaydı yapıldı

---
+++ DataAccess/Context/E_Ticaret_Context.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess.Context
+{
+    public class E_Ticaret_Context : DbContext
+    {
+        public E_Ticaret_Context(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
--- E_Ticaret_Demo/Program.cs
+++ E_Ticaret_Demo/Program.cs
@@ -1,7 +1,12 @@
+using DataAccess.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
+builder.Services.AddDbContext<E_Ticaret_Context>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("ETicaretDb")));
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
