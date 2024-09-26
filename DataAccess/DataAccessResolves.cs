ea5d6bc99bfd46b0ff25192134d47e8212b6d4e2
Sedat Doğan <dogannnsedat@gmail.com>
2024-09-26 12:03:23 +0300

    DataAccess katmanının bağımlılıkları Extension yöntemi ile yapıldı

---
+++ DataAccess/DataAccessResolves.cs
@@ -0,0 +1,21 @@
+﻿using DataAccess.Repository;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccess
+{
+    public static class DataAccessResolves
+    {
+        public static void GetDataAccessServices(this IServiceCollection services)
+        {
+            services.AddScoped<ICategoryReadRepository, CategoryReadRepository>();
+            services.AddScoped<ICategoryWriteRepository, CategoryWriteRepository>();
+            services.AddScoped<IProductReadRepository, ProductReadRepository>();
+            services.AddScoped<IProductWriteRepository, ProductWriteRepository>();
+        }
+    }
+}
--- E_Ticaret_Demo/Program.cs
+++ E_Ticaret_Demo/Program.cs
@@ -1,3 +1,4 @@
+using DataAccess;
 using DataAccess.Context;
 using Microsoft.EntityFrameworkCore;
 
@@ -8,6 +9,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<E_Ticaret_Context>(options =>
 options.UseSqlServer(builder.Configuration.GetConnectionString("ETicaretDb")));
 builder.Services.AddControllers();
+
+builder.Services.GetDataAccessServices();//DataAccess katmanýný servislere ekledik=>IOC
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
