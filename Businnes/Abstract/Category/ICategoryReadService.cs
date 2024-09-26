ee171d38f4494e2b615026b0aceea1e9d48e804d
Sedat Doğan <dogannnsedat@gmail.com>
2024-09-26 14:01:08 +0300

    ICategoryService silindi yerine Write ve Read interface'leri oluşturuldu

---
+++ Businnes/Abstract/Category/ICategoryReadService.cs
@@ -0,0 +1,15 @@
+﻿using Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Businnes
+{
+    public interface ICategoryReadService
+    {
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetSingleCategoryAsync(Guid id);
+    }
+}
--- Businnes/Abstract/Category/ICategoryWriteService.cs
+++ Businnes/Abstract/Category/ICategoryWriteService.cs
@@ -5,18 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Businnes.Abstract
+namespace Businnes
 {
-    public interface ICategoryService
+    public interface ICategoryWriteService
     {
         Task<Category> AddCategoryAsync(string categoryName);
         Task<List<Category>> AddRangeCategoriesAsync(IEnumerable<Category> categories);
-        Task<IEnumerable<Category>> GetCategoriesAsync();
-        Task<Category> GetSingleCategoryAsync(Guid id); 
         Task<Category> DeleteCategoryByIdAsync(Guid id);
         Task<Category> DeleteCategoryAsync(Category category);
         Task<Category> UpdateCategoryAsync(Category category);
-
-
     }
 }
