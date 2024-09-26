8b892ac8760d608b7f87b6ab85d701900971fef6
Sedat Doğan <dogannnsedat@gmail.com>
2024-09-26 13:41:28 +0300

    ICategoryService yazıldı

---
+++ Businnes/Abstract/Category/ICategoryService.cs
@@ -0,0 +1,22 @@
+﻿using Entites;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Businnes.Abstract
+{
+    public interface ICategoryService
+    {
+        Task<Category> AddCategoryAsync(string categoryName);
+        Task<List<Category>> AddRangeCategoriesAsync(IEnumerable<Category> categories);
+        Task<IEnumerable<Category>> GetCategoriesAsync();
+        Task<Category> GetSingleCategoryAsync(Guid id); 
+        Task<Category> DeleteCategoryByIdAsync(Guid id);
+        Task<Category> DeleteCategoryAsync(Category category);
+        Task<Category> UpdateCategoryAsync(Category category);
+
+
+    }
+}
