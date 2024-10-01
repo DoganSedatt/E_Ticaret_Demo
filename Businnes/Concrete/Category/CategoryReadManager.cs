49fd3b3dbf8319d8b532bf454845667e19cf1bee
Sedat Doğan <dogannnsedat@gmail.com>
2024-10-01 18:54:05 +0300

    CategoryReadManager oluşturuldu

---
+++ Businnes/Concrete/Category/CategoryReadManager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Businnes.Concrete.Category
+{
+    public class CategoryReadManager : ICategoryReadService
+    {
+        public Task<IEnumerable<Entites.Category>> GetCategoriesAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Entites.Category> GetSingleCategoryAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
