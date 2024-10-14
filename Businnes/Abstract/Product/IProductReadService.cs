dc0f77ed96220edcfabd63ab557bd3a6e628e377
Sedat Doğan <dogannnsedat@gmail.com>
2024-10-14 22:26:54 +0300

    IProductReadService imza metodlar yazıldı

--- Businnes/Abstract/Product/IProductReadService.cs
+++ Businnes/Abstract/Product/IProductReadService.cs
@@ -1,13 +1,16 @@
-﻿using System;
+﻿using Entites;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Businnes.Abstract.Product
+namespace Businnes.Abstract
 {
     public interface IProductReadService
     {
-
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<IEnumerable<Product>> GetWhereProductsAsync();
+        Task<Product> GetSingleByIdProductAsync(Guid id);
     }
 }
