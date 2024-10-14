f2f270e51a9083cec0784de180f26d2d3f890ecf
Sedat Doğan <dogannnsedat@gmail.com>
2024-10-14 22:31:41 +0300

    ProductReadManager oluşturuldu

---
+++ Businnes/Concrete/Product/ProductReadManager.cs
@@ -0,0 +1,27 @@
+﻿using Businnes.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Businnes.Concrete.Product
+{
+    public class ProductReadManager : IProductReadService
+    {
+        public Task<IEnumerable<Entites.Product>> GetProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Entites.Product> GetSingleByIdProductAsync(Guid id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<IEnumerable<Entites.Product>> GetWhereProductsAsync()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
