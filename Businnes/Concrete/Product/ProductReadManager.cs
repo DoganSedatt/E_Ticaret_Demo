ee7f8c15c97734b943ec151788255def119aed46
Sedat Doğan <dogannnsedat@gmail.com>
2024-10-14 22:44:11 +0300

    ProductReadManager sınıfı yazıldı

--- Businnes/Concrete/Product/ProductReadManager.cs
+++ Businnes/Concrete/Product/ProductReadManager.cs
@@ -1,27 +1,40 @@
 ﻿using Businnes.Abstract;
+using DataAccess.Repository;
+using Entites;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Businnes.Concrete.Product
+namespace Businnes.Concrete
 {
     public class ProductReadManager : IProductReadService
     {
-        public Task<IEnumerable<Entites.Product>> GetProductsAsync()
+        private readonly IProductReadRepository _prodcutReadRepository;
+
+        public ProductReadManager(IProductReadRepository prodcutReadRepository)
         {
-            throw new NotImplementedException();
+            _prodcutReadRepository = prodcutReadRepository;
         }
 
-        public Task<Entites.Product> GetSingleByIdProductAsync(Guid id)
+        public async Task<IEnumerable<Product>> GetProductsAsync()
         {
-            throw new NotImplementedException();
+            IEnumerable<Product> products = _prodcutReadRepository.GetAll().ToList();
+            return products;
         }
 
-        public Task<IEnumerable<Entites.Product>> GetWhereProductsAsync()
+        public async Task<Product> GetSingleByIdProductAsync(Guid id)
         {
-            throw new NotImplementedException();
+            Product? product= await _prodcutReadRepository.GetByIdAsync(id);
+            return product;
+        }
+
+        public async Task<IEnumerable<Product>> GetWhereProductsAsync()
+        {
+            IEnumerable<Product> products = await _prodcutReadRepository.GetWhere(p=>p.ProductName.StartsWith('t')).ToListAsync();
+            return products;
         }
     }
 }
