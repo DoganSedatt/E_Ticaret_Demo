3610cc22d9e870f34ebe55a83de1dea0d7030674
Sedat Doğan <dogannnsedat@gmail.com>
2024-10-15 22:41:49 +0300

    IProductWriteService oluşturuldu

--- Businnes/Abstract/Product/IProductWriteService.cs
+++ Businnes/Abstract/Product/IProductWriteService.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Businnes.Requests.Category;
+using Entites;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Businnes.Abstract.Product
+namespace Businnes.Abstract
 {
     public interface IProductWriteService
     {
-
+        Task<Product> AddCategoryAsync(Product request);
+        Task<List<Product>> AddRangeCategoriesAsync(IEnumerable<Product> categories);
+        Task<Product> DeleteCategoryByIdAsync(Guid id);
+        Task<Product> DeleteCategoryAsync(Product category);
+        Task<Product> UpdateCategoryAsync(Product category);
     }
 }
